1e98e4510d5e076588d9bb130f061ceb07d0b585
doni-zete <donizete112@gmail.com>
2023-12-03 20:20:52 -0300

    feat: add values addlistennerHitbox

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -2,19 +2,37 @@ const state = {
  view: {
   squares: document.querySelectorAll('.square'),
   enemy: document.querySelector('.enemy'),
-  timeLeft: document.querySelector('#time-left'),
   score: document.querySelector('#score'),
+  timeLeft: document.querySelector('#time-left'),
  },
 
-    values: {
-        timerId: null,
-        velocity: 1000
-        
+ values: {
+  velocity: 1000,
+  hitPosition: 0,
+  result: 0,
+  curretTime: 30,
+ },
+ actions: {
+  timerId: setInterval(randomSquare, 1000),
+  countDownTimerId: setInterval(countDown, 1000),
  },
 }
 
-function moveEnemy() {
-    state.values.timerId = setInterval(randomSquare, state.values.velocity)
+function playSound() {
+ let audio = new Audio('../audios/yahoo.m4a')
+ audio.play()
+}
+
+function countDown() {
+ state.values.curretTime--
+ state.view.timeLeft.textContent = state.values.curretTime
+
+ if (state.values.curretTime <= 0) {
+  clearInterval(state.actions.countDownTimerId)
+  clearInterval(state.actions.timerId)
+
+  alert('Game Over! O seu resultado foi: ' + state.values.result)
+ }
 }
 
 function randomSquare() {
@@ -25,14 +43,24 @@ function randomSquare() {
  let randomNumber = Math.floor(Math.random() * 9)
  let randomSquare = state.view.squares[randomNumber]
  randomSquare.classList.add('enemy')
+ state.values.hitPosition = randomSquare.id
 }
 
 function addListenerHitBox() {
- state.view.squares.forEach((square) => {})
+ state.view.squares.forEach((square) => {
+  square.addEventListener('mousedown', () => {
+   if (square.id === state.values.hitPosition) {
+    state.values.result++
+    state.view.score.textContent = state.values.result
+    state.values.hitPosition = null
+    playSound()
+   }
+  })
+ })
 }
 
 function inicio() {
- moveEnemy()
+ addListenerHitBox()
 }
 
 inicio()
